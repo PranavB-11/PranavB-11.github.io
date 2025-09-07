b7acce8492b20fc74e4966271533ad4c97cf6f11
Pranav Banwasi <pranavbanwasi@berkeley.edu>
2025-09-07 10:20:02 -0700

    Project 1

---
+++ Project 1/results.js
@@ -0,0 +1,189 @@
+const resultsData = {
+    "emir.jpg": {
+        "original_name": "emir.tif",
+        "g_disp": [
+            57,
+            20
+        ],
+        "r_disp": [
+            106,
+            41
+        ]
+    },
+    "italil.jpg": {
+        "original_name": "italil.tif",
+        "g_disp": [
+            38,
+            21
+        ],
+        "r_disp": [
+            77,
+            35
+        ]
+    },
+    "monastery.jpg": {
+        "original_name": "monastery.jpg",
+        "g_disp": [
+            -3,
+            2
+        ],
+        "r_disp": [
+            3,
+            2
+        ]
+    },
+    "church.jpg": {
+        "original_name": "church.tif",
+        "g_disp": [
+            25,
+            4
+        ],
+        "r_disp": [
+            58,
+            -4
+        ]
+    },
+    "three_generations.jpg": {
+        "original_name": "three_generations.tif",
+        "g_disp": [
+            52,
+            14
+        ],
+        "r_disp": [
+            111,
+            12
+        ]
+    },
+    "lugano.jpg": {
+        "original_name": "lugano.tif",
+        "g_disp": [
+            41,
+            -16
+        ],
+        "r_disp": [
+            93,
+            -29
+        ]
+    },
+    "melons.jpg": {
+        "original_name": "melons.tif",
+        "g_disp": [
+            81,
+            10
+        ],
+        "r_disp": [
+            178,
+            14
+        ]
+    },
+    "mosque.jpg": {
+        "original_name": "mosque.tif",
+        "g_disp": [
+            25,
+            10
+        ],
+        "r_disp": [
+            70,
+            11
+        ]
+    },
+    "lastochikino.jpg": {
+        "original_name": "lastochikino.tif",
+        "g_disp": [
+            -3,
+            -2
+        ],
+        "r_disp": [
+            75,
+            -9
+        ]
+    },
+    "train.jpg": {
+        "original_name": "train.tif",
+        "g_disp": [
+            43,
+            6
+        ],
+        "r_disp": [
+            87,
+            32
+        ]
+    },
+    "tobolsk.jpg": {
+        "original_name": "tobolsk.jpg",
+        "g_disp": [
+            3,
+            3
+        ],
+        "r_disp": [
+            7,
+            3
+        ]
+    },
+    "icon.jpg": {
+        "original_name": "icon.tif",
+        "g_disp": [
+            40,
+            17
+        ],
+        "r_disp": [
+            89,
+            23
+        ]
+    },
+    "cathedral.jpg": {
+        "original_name": "cathedral.jpg",
+        "g_disp": [
+            5,
+            2
+        ],
+        "r_disp": [
+            12,
+            3
+        ]
+    },
+    "siren.jpg": {
+        "original_name": "siren.tif",
+        "g_disp": [
+            49,
+            -6
+        ],
+        "r_disp": [
+            96,
+            -24
+        ]
+    },
+    "lake.jpg": {
+        "original_name": "lake.tif",
+        "g_disp": [
+            15,
+            -7
+        ],
+        "r_disp": [
+            82,
+            -16
+        ]
+    },
+    "self_portrait.jpg": {
+        "original_name": "self_portrait.tif",
+        "g_disp": [
+            78,
+            29
+        ],
+        "r_disp": [
+            176,
+            37
+        ]
+    },
+    "harvesters.jpg": {
+        "original_name": "harvesters.tif",
+        "g_disp": [
+            59,
+            17
+        ],
+        "r_disp": [
+            123,
+            14
+        ]
+    }
+};
